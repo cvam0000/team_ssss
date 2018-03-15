e90b750da48960505994bf1445e79159d0fd2bda
root <root@localhost.localdomain>
2018-03-16 00:13:26 +0530

    first commit

---
+++ logined.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+  <meta charset="UTF-8">
+  <title>Login/Sign-In</title>
+  </head>
+  <body>
+  <?php
+  echo "welcome".$_SESSION['username'];
+  ?>
+  </body>
+  </html>
